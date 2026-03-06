54ce24d3a6b628d53465ff37c249bb3992b2dcfb
qcbjewelers <jaime@qcbjewelers.com>
2026-03-06 15:16:49 -0800

    Update diamonds.js

--- api/diamonds.js
+++ api/diamonds.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
   const page = req.query.page || 1;
 
-  const url = `https://lgdusallc.com/developer-api/diamond?type=certified&page=${page}&key=3555e3505b955d1dcf363ad6f03a8d51ca3`;
+  const url = `https://lgdusallc.com/developer-api/diamond?type=certified&page=${page}&key=3555e3505b955d1dcf363ad6f03a8d51ca4e4b3c3b7b`;
 
   try {
 
